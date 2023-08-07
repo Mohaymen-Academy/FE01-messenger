a4d0bceea70a340b1747a62781a3ee1e19ebf031
Mahdi Pourtalebi <mahdi.pourtalebi@gmail.com>
2023-08-07 13:41:48 +0330

    initial

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,15 +1,11 @@
-import LeftSection from '../LeftSection/LeftSection'
-import MidSection from '../MidSection'
 import RightSection from '../RightSection/RightSection'
 
 interface MainPageProps {}
 
 export default function MainPage({}: MainPageProps) {
   return (
-    <div className="relative flex w-full h-screen overflow-hidden antialiased bg-gray-200">
+    <div className="relative flex h-screen w-full overflow-hidden bg-gray-200 antialiased">
       <RightSection />
-      <MidSection />
-      <LeftSection />
     </div>
   )
 }
--- src/components/RightSection/RightSection.tsx
+++ src/components/RightSection/RightSection.tsx
@@ -1,6 +1,4 @@
 import IconButton from '../Common/IconButton/IconButton'
-import TabButton from '../Common/TabButton/TabButton'
-import ChatBox from './ChatBox'
 import ChatList from './ChatList'
 import ChatNav from './ChatNav'
 import RightHeader from './RightHeader/RightHeader'
@@ -10,7 +8,7 @@ interface RightSectionProps {}
 export default function RightSection({}: RightSectionProps) {
   return (
     <div>
-      <div className="bg-white relative h-full md:block w-96 border-r border-gray-300 shadow-xl transform transition-all duration-500 ease-in-out">
+      <div className="relative h-full w-96 border-r border-gray-300 bg-white shadow-xl transition-all duration-500 ease-in-out md:block">
         <RightHeader />
         <ChatNav />
         <ChatList />
