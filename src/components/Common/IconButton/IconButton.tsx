0bd5a017dbfdb762e7e623c8615f0307ee30edb6
Mahdi Pourtalebi <mahdi.pourtalebi@gmail.com>
2023-08-08 10:57:59 +0330

    added type to iconbutton

--- src/components/Common/IconButton/IconButton.tsx
+++ src/components/Common/IconButton/IconButton.tsx
@@ -2,12 +2,16 @@ import { ReactElement } from 'react'
 
 interface IconButtonProps {
   icon: ReactElement
+  type?: 'button' | 'submit' | 'reset'
 }
 
-export default function IconButton({ icon }: IconButtonProps) {
+export default function IconButton({ icon, type = 'button' }: IconButtonProps) {
   return (
     <div>
-      <button className="rounded-full p-2 text-gray-500 hover:bg-gray-200 hover:text-gray-600 focus:outline-none">
+      <button
+        type={type}
+        className="rounded-full p-2 text-gray-500 hover:bg-gray-200 hover:text-gray-600 focus:outline-none"
+      >
         {icon}
       </button>
     </div>
