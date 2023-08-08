8ed38cd775cd2e4015f5e0916d09508813aee4cb
Hossein-Asadi <hosseinasd0109@gmail.com>
2023-08-08 15:12:39 +0330

    Add log out feature

--- src/components/RightSection/SettingsColumn/SettingsColumn.tsx
+++ src/components/RightSection/SettingsColumn/SettingsColumn.tsx
@@ -1,10 +1,12 @@
-import { FiAtSign } from 'react-icons/fi'
-import { BsTelephone, BsInfoCircle } from 'react-icons/bs'
+import { FiAtSign, FiLogOut } from 'react-icons/fi'
+import { BsTelephone, BsInfoCircle, BsFillMoonStarsFill } from 'react-icons/bs'
+import { FaSun } from 'react-icons/fa'
 import InfoImage from '@/components/Common/InfoImage'
 import InfoRow from '@/components/Common/InfoRow'
 import InfoHeader from '@/components/LeftSection/DetailsColumn/InfoHeader'
 import img from '@/assets/download.jpeg'
 import SettingsHeader from '../SettingsHeader/SettingsHeader'
+
 
 interface SettingsColumnProps {
   isActive: boolean
@@ -37,6 +39,24 @@ export default function SettingsColumn({
         subTitle="بیوگرافی"
         icon={<BsInfoCircle className="h-6 w-6 fill-current text-gray-600" />}
       />
+      <button className="mr-4 mt-8 flex w-[355px] justify-start gap-2 rounded-lg py-2 pr-1  transition-all duration-300 ease-in-out ">
+        <div className="fill-current">
+          <BsFillMoonStarsFill className={classNames("h-6 w-6",)} />
+          <FaSun className="hidden h-6 w-6" />
+        </div>
+        <div className="text-slate-700">
+          <p>تغییر به حالت شب</p>
+        </div>
+      </button>
+
+      <button className="mr-3 mt-2 flex w-[355px] justify-start gap-2 rounded-lg py-2 pr-1 text-red-500 transition-all duration-300 ease-in-out hover:bg-red-500 hover:text-white">
+        <div className="fill-current">
+          <FiLogOut className="h-7 w-7" />
+        </div>
+        <div>
+          <p>خروج از حساب کاربری</p>
+        </div>
+      </button>
     </div>
   )
 }
